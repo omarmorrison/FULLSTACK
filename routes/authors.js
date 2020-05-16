246ca05819b28ce940b657c58d04fe528c7665e9
Omar Morrison <omar.morrison@ribbitwise.com>
2020-05-16 13:33:35 -0400

    added search

--- routes/authors.js
+++ routes/authors.js
@@ -1,20 +1,39 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
 
 
 // All Authors
-router.get('/', (req, res) => {
-    res.render('authors/index')
+router.get('/', async (req, res) => {
+    try {
+        const authors = await Author.find({})
+        res.render('authors/index', { authors: authors})
+    } catch {
+        res.redirect('/')
+    }
 })
 
 // New Authors
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 })
 
-// Create an Author
-router.post('/', (req, res) => {
-    res.render('Create')
+// Create an Author Route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save() 
+        //res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch{
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+
+    }
 })
 
 module.exports = router
