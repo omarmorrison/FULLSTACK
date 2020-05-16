0f70ef619b67900074a634627944bcc1d8c0dc37
Omar Morrison <omar.morrison@ribbitwise.com>
2020-05-16 13:59:03 -0400

    Initial Author Routes

--- routes/authors.js
+++ routes/authors.js
@@ -5,9 +5,16 @@ const Author = require('../models/author')
 
 // All Authors
 router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
     try {
-        const authors = await Author.find({})
-        res.render('authors/index', { authors: authors})
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { 
+            authors: authors, 
+            searchOptions: req.query 
+        })
     } catch {
         res.redirect('/')
     }
@@ -18,7 +25,7 @@ router.get('/new', (req, res) => {
     res.render('authors/new', { author: new Author() })
 })
 
-// Create an Author Route
+// Create Author Route
 router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name
@@ -32,7 +39,6 @@ router.post('/', async (req, res) => {
             author: author,
             errorMessage: 'Error creating Author'
         })
-
     }
 })
 
