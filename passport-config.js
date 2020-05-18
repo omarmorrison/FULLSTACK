a86dce9b387b17e41d0772592eccb439cc9bcb50
Omar Morrison <omar.morrison@ribbitwise.com>
2020-05-17 20:33:07 -0400

    removed all ref of bcrypt

--- passport-config.js
+++ passport-config.js
@@ -1,3 +1,4 @@
+/*
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
@@ -27,3 +28,4 @@ function initialize(passport, getUserByEmail, getUserById){
 }
 
 module.exports = initialize
+*/
