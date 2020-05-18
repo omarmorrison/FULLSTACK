d589316593ea26040de9e9a6027c1cfce3233f70
Omar Morrison <omar.morrison@ribbitwise.com>
2020-05-17 20:34:55 -0400

    remove passport-config

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
 
+/*
 const initializePassport = require('./passport-config')
 initializePassport(
     passport, 
     email => users.find(user => user.email === email),
     id => users.find(user => user.id === id) 
 )
+*/
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
